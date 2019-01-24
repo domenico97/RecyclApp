4a3116cbe8a7074f3341b9ab65e54db1477189b0
Domenico Trotta <43494171+domenico97@users.noreply.github.com>
2019-01-24 08:31:51 +0100

    Rinominati png con lettere piccole

--- app/src/main/java/com/example/domenico/myapp/HomepageCittadino.java
+++ app/src/main/java/com/example/domenico/myapp/HomepageCittadino.java
@@ -1,36 +1,21 @@
 package com.example.domenico.myapp;
 
-import android.app.ActionBar;
-import android.app.Activity;
-import android.content.ContentValues;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.synnapps.carouselview.CarouselView;
-import com.synnapps.carouselview.ImageListener;
 import com.synnapps.carouselview.ViewListener;
-
-import java.util.ArrayList;
 
 public class HomepageCittadino extends AppCompatActivity {
 
     CarouselView customCarouselView;
     int NUMBER_OF_PAGES = 2;
     TextView testo;
+    boolean prima_pagina = true;
     ImageButton image;
 
     int[] sampleImages = {R.drawable.calendar, R.drawable.info, R.drawable.home};
@@ -53,15 +38,20 @@ public class HomepageCittadino extends AppCompatActivity {
         @Override
         public View setViewForPosition(int position) {
             View customView = getLayoutInflater().inflate(R.layout.view_custom, null);
-            testo = (TextView) findViewById(R.id.testo);
-            image = findViewById(R.id.image);
+            testo = customView.findViewById(R.id.testo);
+            image = customView.findViewById(R.id.image);
             if (testo != null && image != null) {
-                testo.setText("NOME UTENTE oggi si conferisce");
-                image.setImageResource(sampleImages[position]);
-                //set view attributes here
+                if (position==0) {
+                    testo.setText("NOME UTENTE oggi si conferisce");
+                    image.setImageResource(R.drawable.alluminio);
+
+                } else {
+                    testo.setText("Continua così, NOME UTENTE");
+                    image.setImageResource(R.drawable.alluminio);
+                }
             }
-            if(testo!=null)
-            Toast.makeText(getApplicationContext(), "" + testo.getText().toString(), Toast.LENGTH_LONG).show();
+            if (testo != null)
+                Toast.makeText(getApplicationContext(), "" + testo.getText().toString(), Toast.LENGTH_LONG).show();
             return customView;
         }
     };
