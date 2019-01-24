159e24981972145b537f5071c67235c1861d0696
mcastellaneta <43062263+mcastellaneta@users.noreply.github.com>
2019-01-24 11:56:59 +0100

    Aggiunta homepage Operatore Ecologico e png inerenti

---
+++ app/src/main/java/com/example/domenico/myapp/HomePageOperatoreEcologico.java
@@ -0,0 +1,19 @@
+package com.example.domenico.myapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.synnapps.carouselview.CarouselView;
+
+public class HomePageOperatoreEcologico extends Activity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page_cittadino);
+
+
+
+    }
+
+}
