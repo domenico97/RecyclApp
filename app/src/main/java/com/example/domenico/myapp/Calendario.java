620412ff0b7539817bd4dfd6af654df16edcbd3d
Domenico Trotta <43494171+domenico97@users.noreply.github.com>
2019-01-25 09:01:57 +0100

    modifiche

--- app/src/main/java/com/example/domenico/myapp/Calendario.java
+++ app/src/main/java/com/example/domenico/myapp/Calendario.java
@@ -4,11 +4,12 @@ import android.app.Activity;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.SimpleCursorAdapter;
 
 import java.util.ArrayList;
 
-public class Calendario extends Activity {
+public class Calendario extends AppCompatActivity {
 
 
     @Override
