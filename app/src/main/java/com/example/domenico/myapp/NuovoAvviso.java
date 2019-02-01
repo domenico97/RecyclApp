48e44b6d7e59f850bc5ec42f94981fc4e7082411
Domenico Trotta <43494171+domenico97@users.noreply.github.com>
2019-02-01 12:26:33 +0100

    add nuovoAvviso

--- app/src/main/java/com/example/domenico/myapp/NuovoAvviso.java
+++ app/src/main/java/com/example/domenico/myapp/NuovoAvviso.java
@@ -1,19 +1,30 @@
 package com.example.domenico.myapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class NuovoAvviso extends Activity {
     private BottomNavigationView bottomNavigationView;
+    private EditText nome,descrizione;
+    private DatabaseOpenHelper dbHelper;
+    private SQLiteDatabase db = null;
+    SharedPreferences prefs;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nuovo_avviso);
-
+        nome = findViewById(R.id.titolo1);
         bottomNavigationView = findViewById(R.id.navigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -37,6 +48,63 @@ public class NuovoAvviso extends Activity {
             }
         });
     }
+
+
+    public void annulla(View v) {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        Intent i = new Intent();
+                        i.setClass(getApplicationContext(), HomepageDipendenteComunale.class);
+                        startActivity(i);
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Annulla");
+        builder.setMessage("Sei sicuro di voler annullare l'avviso ? ")
+                .setPositiveButton("Si", dialogClickListener)
+                .setNegativeButton("No", dialogClickListener).show();
+
+        return;
+    }
+
+    public void salva(View v) {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        String nomeAvviso = nome.getText().toString();
+                       /* ContentValues values = new ContentValues();
+                        values.put(SchemaDB.Tavola.COLUMN_TIPO_SEGNALAZIONE, "");
+                        values.put(SchemaDB.Tavola.COLUMN_MESSAGGIO, descr);
+                        values.put(SchemaDB.Tavola.COLUMN_OGGETTO, nomeAvviso);
+                        values.put(SchemaDB.Tavola.COLUMN_MITTENTE, cf);
+                        values.put(SchemaDB.Tavola.COLUMN_TIPO, "dip comunale"); //Tipologia di attori a cui è rivolta la segnalazione
+                        values.put(SchemaDB.Tavola.COLUMN_DATA_SEGNALAZIONE, data);
+                        values.put(SchemaDB.Tavola.COLUMN_DESTINATARIO, "");
+                        db.insert(SchemaDB.Tavola.TABLE_NAME1, null, values);*/
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Salva avviso");
+        builder.setMessage("Sei sicuro di voler salvare il nuovo avviso ? ")
+                .setPositiveButton("Si", dialogClickListener)
+                .setNegativeButton("No", dialogClickListener).show();
+
+        return;
+    }
+
 
 }
 
