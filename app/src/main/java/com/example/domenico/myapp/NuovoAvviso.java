12dc309a40ffc4e61704682d1b0ad587bffd22b6
Domenico Trotta <43494171+domenico97@users.noreply.github.com>
2019-02-01 13:13:04 +0100

    add nuovoAvviso

--- app/src/main/java/com/example/domenico/myapp/NuovoAvviso.java
+++ app/src/main/java/com/example/domenico/myapp/NuovoAvviso.java
@@ -6,25 +6,41 @@ import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class NuovoAvviso extends Activity {
     private BottomNavigationView bottomNavigationView;
-    private EditText nome,descrizione;
+    private EditText nome, descrizione;
     private DatabaseOpenHelper dbHelper;
+    private CheckBox cittadinoCheck, operatoreCheck;
+    String cittadino = "";
+    String operatore = "";
     private SQLiteDatabase db = null;
     SharedPreferences prefs;
+    boolean cittadinoCheched = false;
+    boolean operatoreCheched = false;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nuovo_avviso);
         nome = findViewById(R.id.titolo1);
+        descrizione = findViewById(R.id.descrizione);
+        db = MainActivity.dbHelper.getWritableDatabase();
+        cittadinoCheck = findViewById(R.id.cittadinoCheck);
+        operatoreCheck = findViewById(R.id.operatoreCheck);
+
         bottomNavigationView = findViewById(R.id.navigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -50,6 +66,124 @@ public class NuovoAvviso extends Activity {
     }
 
 
+    public void onCittadinoClick(View v) {
+        if (cittadinoCheched) {
+            cittadinoCheched = false;
+        } else {
+            cittadinoCheched = true;
+        }
+    }
+
+    public void onOperatoreClick(View v) {
+        if (operatoreCheched) {
+            operatoreCheched = false;
+        } else {
+            operatoreCheched = true;
+        }
+    }
+
+
+    public void salva(View v) {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+
+                        Calendar cal = Calendar.getInstance();
+                        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+                        String data = sdf.format(cal.getTime());
+                        prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                        int id = prefs.getInt("ID", 0);
+                        String cf = null;
+                        Cursor c = db.rawQuery("SELECT cf FROM  utenti where id = ?", new String[]{"" + id});
+                        if (c.moveToLast()) {
+                            cf = c.getString(0);
+                        }
+                        String nomeAvviso = nome.getText().toString();
+                        String descr = descrizione.getText().toString();
+                        if (cittadinoCheched) {
+                            ContentValues values = new ContentValues();
+                            values.put(SchemaDB.Tavola.COLUMN_TIPO_SEGNALAZIONE, "");
+                            values.put(SchemaDB.Tavola.COLUMN_MESSAGGIO, descr);
+                            values.put(SchemaDB.Tavola.COLUMN_OGGETTO, nomeAvviso);
+                            values.put(SchemaDB.Tavola.COLUMN_MITTENTE, cf);
+                            values.put(SchemaDB.Tavola.COLUMN_TIPO, "cittadino"); //Tipologia di attori a cui è rivolta la segnalazione
+                            values.put(SchemaDB.Tavola.COLUMN_DATA_SEGNALAZIONE, data);
+                            values.put(SchemaDB.Tavola.COLUMN_DESTINATARIO, "");
+                            db.insert(SchemaDB.Tavola.TABLE_NAME1, null, values);
+                        }
+                        if (operatoreCheched) {
+                            ContentValues values = new ContentValues();
+                            values.put(SchemaDB.Tavola.COLUMN_TIPO_SEGNALAZIONE, "");
+                            values.put(SchemaDB.Tavola.COLUMN_MESSAGGIO, descr);
+                            values.put(SchemaDB.Tavola.COLUMN_OGGETTO, nomeAvviso);
+                            values.put(SchemaDB.Tavola.COLUMN_MITTENTE, cf);
+                            values.put(SchemaDB.Tavola.COLUMN_TIPO, "op ecologico"); //Tipologia di attori a cui è rivolta la segnalazione
+                            values.put(SchemaDB.Tavola.COLUMN_DATA_SEGNALAZIONE, data);
+                            values.put(SchemaDB.Tavola.COLUMN_DESTINATARIO, "");
+                            db.insert(SchemaDB.Tavola.TABLE_NAME1, null, values);
+                        }
+                        if (cittadinoCheched == false && operatoreCheched == false) {
+                            errore();
+                        } else
+                            invioEffettuato();
+
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Salva avviso");
+        builder.setMessage("Sei sicuro di voler salvare il nuovo avviso ? ")
+                .setPositiveButton("Si", dialogClickListener)
+                .setNegativeButton("No", dialogClickListener).show();
+
+        return;
+    }
+
+    private void invioEffettuato() {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        break;
+
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Notifica");
+        builder.setMessage("L'avviso è stato inviato ")
+                .setPositiveButton("Ho capito", dialogClickListener).show();
+
+        return;
+
+    }
+
+    private void errore() {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Attenzione");
+        builder.setMessage("Nessun destinatario selezionato ! ")
+                .setPositiveButton("Ho capito", dialogClickListener).show();
+
+        return;
+
+    }
+
+
     public void annulla(View v) {
         DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
@@ -68,37 +202,6 @@ public class NuovoAvviso extends Activity {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Annulla");
         builder.setMessage("Sei sicuro di voler annullare l'avviso ? ")
-                .setPositiveButton("Si", dialogClickListener)
-                .setNegativeButton("No", dialogClickListener).show();
-
-        return;
-    }
-
-    public void salva(View v) {
-        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                switch (which) {
-                    case DialogInterface.BUTTON_POSITIVE:
-                        String nomeAvviso = nome.getText().toString();
-                       /* ContentValues values = new ContentValues();
-                        values.put(SchemaDB.Tavola.COLUMN_TIPO_SEGNALAZIONE, "");
-                        values.put(SchemaDB.Tavola.COLUMN_MESSAGGIO, descr);
-                        values.put(SchemaDB.Tavola.COLUMN_OGGETTO, nomeAvviso);
-                        values.put(SchemaDB.Tavola.COLUMN_MITTENTE, cf);
-                        values.put(SchemaDB.Tavola.COLUMN_TIPO, "dip comunale"); //Tipologia di attori a cui è rivolta la segnalazione
-                        values.put(SchemaDB.Tavola.COLUMN_DATA_SEGNALAZIONE, data);
-                        values.put(SchemaDB.Tavola.COLUMN_DESTINATARIO, "");
-                        db.insert(SchemaDB.Tavola.TABLE_NAME1, null, values);*/
-                        break;
-                    case DialogInterface.BUTTON_NEGATIVE:
-                        break;
-                }
-            }
-        };
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Salva avviso");
-        builder.setMessage("Sei sicuro di voler salvare il nuovo avviso ? ")
                 .setPositiveButton("Si", dialogClickListener)
                 .setNegativeButton("No", dialogClickListener).show();
 
