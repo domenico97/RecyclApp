f7164579ef11b6737b140f49f6b4cf800f73f0c8
mcastellaneta <43062263+mcastellaneta@users.noreply.github.com>
2019-02-04 09:45:02 +0100

    Modificato tasto nuovo avvio ed creato tasto invia per l'invio dell'infrazione al cittadino

--- app/src/main/java/com/example/domenico/myapp/DettagliSegnalazioneDCOperatore.java
+++ app/src/main/java/com/example/domenico/myapp/DettagliSegnalazioneDCOperatore.java
@@ -5,13 +5,17 @@ import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -20,19 +24,23 @@ import java.util.Calendar;
 public class DettagliSegnalazioneDCOperatore extends Activity {
 
     TextView cfSegnalatore,dataSegnalazione,cfSegnalato,descrizioneInfrazione;
-    String infrazioni;
+    String infrazioni,cfDaSegnalare,dataInfrazione;
+    FrameLayout inviaButton;
     private BottomNavigationView bottomNavigationView;
     private DatabaseOpenHelper dbHelper;
     private SQLiteDatabase db = null;
+    SharedPreferences prefs;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dettagli_segnalazione_dc_operatore);
 
+        prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         cfSegnalatore = findViewById(R.id.cfSegnalatore);
         dataSegnalazione = findViewById(R.id.dataSegnalazione);
         cfSegnalato = findViewById(R.id.cfSegnalato);
         descrizioneInfrazione = findViewById(R.id.descrizioneInfrazione);
+        inviaButton = findViewById(R.id.inviaButton);
 
         db = MainActivity.dbHelper.getWritableDatabase();
 
@@ -62,8 +70,10 @@ public class DettagliSegnalazioneDCOperatore extends Activity {
 
         Intent i = getIntent();
         cfSegnalatore.setText(i.getStringExtra("mittente"));
-        dataSegnalazione.setText(i.getStringExtra("data"));
-        cfSegnalato.setText(i.getStringExtra("destinatario"));
+        dataInfrazione = i.getStringExtra("data");
+        dataSegnalazione.setText(dataInfrazione);
+        cfDaSegnalare = i.getStringExtra("destinatario");
+        cfSegnalato.setText(cfDaSegnalare);
         infrazioni = i.getStringExtra("messaggio");
 
 
@@ -88,14 +98,44 @@ public class DettagliSegnalazioneDCOperatore extends Activity {
         }
 
         descrizioneInfrazione.setText(testo);
+        checkInfrazioneInviata();
     }
 
+    private void checkInfrazioneInviata() {
+
+
+        Cursor c = db.rawQuery("SELECT tipo_segnalazione,data_segnalazione,destinatario,oggetto,mittente,messaggio FROM messaggi where tipo = ?", new String[]{"cittadino"});
+        if (c != null && c.getCount() > 0) {
+            for (int j = 0; j < c.getCount(); j++) {
+                if (c.moveToPosition(j)) {
+                    Log.d("DBUG", "ciSta");
+                    String tipo_segnalazione = c.getString(0);
+                    String data = c.getString(1);
+                    String destinatario = c.getString(2);
+                    String oggetto = c.getString(3);
+                    String mittente = c.getString(4);
+                    String messaggio = c.getString(5);
+                    Log.d("DBUG", tipo_segnalazione + data + destinatario + oggetto + messaggio);
+                    if (tipo_segnalazione.equals("Sanzione") && data.equals(dataInfrazione) && destinatario.equals(cfDaSegnalare) && oggetto.equals("Sanzione") && messaggio.equals(infrazioni)) {
+                        Log.d("DBUG", "giaInviata");
+                        inviaButton.setVisibility(View.INVISIBLE);
+                        return;
+                    }
+
+
+                }
+            }
+        }
+
+
+
+    }
 
 
     public void inviaInfrazione(View view) {
 
-
-/*
+            int id = prefs.getInt("ID",0);
+            String codiceFiscale = "Assessorato all'ambiente";
             Cursor c = db.rawQuery("SELECT cf FROM utenti where id = ?", new String[]{"" + id});
             if (c != null && c.getCount() > 0) {
                 if (c.moveToFirst()) {
@@ -104,21 +144,19 @@ public class DettagliSegnalazioneDCOperatore extends Activity {
 
                 }
             }
-            Calendar cal = Calendar.getInstance();
-            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-            String data = sdf.format(cal.getTime());
+
 
 
             ContentValues values = new ContentValues();
             values.put(SchemaDB.Tavola.COLUMN_TIPO_SEGNALAZIONE, "Sanzione");
-            values.put(SchemaDB.Tavola.COLUMN_MESSAGGIO, descr);
+            values.put(SchemaDB.Tavola.COLUMN_MESSAGGIO, infrazioni);
             values.put(SchemaDB.Tavola.COLUMN_OGGETTO, "Sanzione");
             values.put(SchemaDB.Tavola.COLUMN_MITTENTE, codiceFiscale);
             values.put(SchemaDB.Tavola.COLUMN_TIPO, "cittadino");
-            values.put(SchemaDB.Tavola.COLUMN_DATA_SEGNALAZIONE, data);
-            values.put(SchemaDB.Tavola.COLUMN_DESTINATARIO, cf.getText().toString());
-            db.insert(SchemaDB.Tavola.TABLE_NAME1, null, values);
-            confermaInvio();  */
+            values.put(SchemaDB.Tavola.COLUMN_DATA_SEGNALAZIONE, dataInfrazione);
+            values.put(SchemaDB.Tavola.COLUMN_DESTINATARIO, cfDaSegnalare);
+            Log.d("DBUG",""+db.insert(SchemaDB.Tavola.TABLE_NAME1, null, values));
+            confermaInvio();
 
     }
 
