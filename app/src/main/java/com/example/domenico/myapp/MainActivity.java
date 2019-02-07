7d174ef6ceadf9240fce0bb971334c6bb04fab09
Domenico Trotta <43494171+domenico97@users.noreply.github.com>
2019-02-07 18:06:12 +0100

    Fine

--- app/src/main/java/com/example/domenico/myapp/MainActivity.java
+++ app/src/main/java/com/example/domenico/myapp/MainActivity.java
@@ -26,8 +26,8 @@ public class MainActivity extends Activity {
     private SimpleCursorAdapter adapterSelected;
     SharedPreferences prefs;
     ArrayList<Utente> utenti = new ArrayList<>();
-    String[] giorni = {"Sun","Mon_Odd","Mon_Even","Tue","Wed","Thu","Fri","Sat"};
-    String[] tipologia = {"umido","indifferenziato","alluminio","umido","plastica","cartonevetro","umido","nonconferire"};
+    String[] giorni = {"Sun", "Mon_Odd", "Mon_Even", "Tue", "Wed", "Thu", "Fri", "Sat"};
+    String[] tipologia = {"umido", "indifferenziato", "alluminio", "umido", "plastica", "cartonevetro", "umido", "nonconferire"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +39,13 @@ public class MainActivity extends Activity {
         if (primoAccesso) {
             db = dbHelper.getWritableDatabase();
             utenti.add(new Utente("Giulia", "Valli", "VLLGLI79A41H703B", "giulia.valli@gmail.com", "via Roma Fisciano", "giulia79", "3894552124", "dip comunale"));
-            utenti.add(new Utente("Domenico", "Trotta", "VLLGLI79A41H703C", "domenico.trotta@live.it", "via Roma Fisciano", "dom1997", "3894552124", "cittadino"));
+            utenti.add(new Utente("Laura", "Bianchi", "LRTBNC88L17D789F", "laura.bianchi@gmail.com", "via Pendino Fisciano", "laura93", "3274552852", "cittadino"));
+            utenti.add(new Utente("Domenico", "Trotta", "VLLGLI79A41H703C", "domenico.trotta@live.it", "via Maurizio Fisciano", "dom1997", "3894552124", "cittadino"));
             utenti.add(new Utente("Marco", "Giuliani", "GLNMRC74M06H703X", "marco.giuliani@gmail.com", "via Toscanello Baronissi", "marco74", "3297856896", "op ecologico"));
-            Messaggio x = new Messaggio(0, "ciao", "VLLGLI79A41H703B", "cittadino", "22/10/2018", "PROVA", "VLLGLI79A41H703C", "Multa");
-            Messaggio y = new Messaggio(1, "avviso", "VLLGLI79A41H703B", "op ecologico", "22/10/2018", "Avviso", "", "Multa");
+            // Messaggio x = new Messaggio(0, "ciao", "VLLGLI79A41H703B", "cittadino", "22/10/2018", "PROVA", "VLLGLI79A41H703C", "Multa");
+            //  Messaggio y = new Messaggio(1, "avviso", "VLLGLI79A41H703B", "op ecologico", "22/10/2018", "Avviso", "", "Multa");
 
-            ContentValues values = new ContentValues();
+           /* ContentValues values = new ContentValues();
             values.put(SchemaDB.Tavola.COLUMN_DATA_SEGNALAZIONE, x.getData());
             values.put(SchemaDB.Tavola.COLUMN_MITTENTE, x.getMittente());
             values.put(SchemaDB.Tavola.COLUMN_OGGETTO, x.getOggetto());
@@ -53,25 +54,6 @@ public class MainActivity extends Activity {
             values.put(SchemaDB.Tavola.COLUMN_TIPO_SEGNALAZIONE, x.getTipo_segnalazione());
             values.put(SchemaDB.Tavola.COLUMN_MESSAGGIO, x.getMessaggio());
             db.insert(SchemaDB.Tavola.TABLE_NAME1, null, values);
-
-
-            values = new ContentValues();
-            values.put(SchemaDB.Tavola.COLUMN_CF, "TREDFG88J17F874G");
-            values.put(SchemaDB.Tavola.COLUMN_NAME, "FRANCO");
-            values.put(SchemaDB.Tavola.COLUMN_COGNOME, "BBBB");
-            db.insert(SchemaDB.Tavola.TABLE_NAME2, null, values);
-
-            values = new ContentValues();
-            values.put(SchemaDB.Tavola.COLUMN_CF, "TREDFG88J17F87F4");
-            values.put(SchemaDB.Tavola.COLUMN_NAME, "LUCA");
-            values.put(SchemaDB.Tavola.COLUMN_COGNOME, "ROSSI");
-            db.insert(SchemaDB.Tavola.TABLE_NAME2, null, values);
-
-            values = new ContentValues();
-            values.put(SchemaDB.Tavola.COLUMN_CF, "TREDFG88J17F887D");
-            values.put(SchemaDB.Tavola.COLUMN_NAME, "AAAAA");
-            values.put(SchemaDB.Tavola.COLUMN_COGNOME, "BIANCHI");
-            db.insert(SchemaDB.Tavola.TABLE_NAME2, null, values);
 
 
             ContentValues valori = new ContentValues();
@@ -83,11 +65,11 @@ public class MainActivity extends Activity {
             valori.put(SchemaDB.Tavola.COLUMN_TIPO_SEGNALAZIONE, y.getTipo_segnalazione());
             valori.put(SchemaDB.Tavola.COLUMN_MESSAGGIO, y.getMessaggio());
             long controllo = db.insert(SchemaDB.Tavola.TABLE_NAME1, null, valori);
-            Log.d("PROVA",controllo+"");
+            Log.d("PROVA",controllo+"");*/
 
 
             for (int i = 0; i < utenti.size(); i++) {
-                values = new ContentValues();
+                ContentValues values = new ContentValues();
                 values.put(SchemaDB.Tavola.COLUMN_NAME, utenti.get(i).getNome());
                 values.put(SchemaDB.Tavola.COLUMN_COGNOME, utenti.get(i).getCognome());
                 values.put(SchemaDB.Tavola.COLUMN_CF, utenti.get(i).getCf());
@@ -99,12 +81,12 @@ public class MainActivity extends Activity {
                 db.insert(SchemaDB.Tavola.TABLE_NAME, null, values);
             }
 
-            for(int i=0; i< giorni.length;i++){
+            for (int i = 0; i < giorni.length; i++) {
                 ContentValues cv = new ContentValues();
-                cv.put(SchemaDB.Tavola.CALENDARIO_GIORNO,giorni[i]);
-                cv.put(SchemaDB.Tavola.CALENDARIO_TIPOLOGIA,tipologia[i]);
+                cv.put(SchemaDB.Tavola.CALENDARIO_GIORNO, giorni[i]);
+                cv.put(SchemaDB.Tavola.CALENDARIO_TIPOLOGIA, tipologia[i]);
                 long test = db.insert(SchemaDB.Tavola.CALENDARIO, null, cv);
-                Log.d("TEST",""+test);
+                Log.d("TEST", "" + test);
 
             }
 
@@ -137,25 +119,25 @@ public class MainActivity extends Activity {
                     connesso = c.getInt(0);
 
                 }*/
-                Intent z = new Intent();
-                if (prefs.getBoolean("RIMANI_CONNESSO", false)==true) {
-                    if (prefs.getString("TIPO", "").equals("cittadino")) {
-                        z.setClass(getApplicationContext(), HomepageCittadino.class);
-                    } else if (prefs.getString("TIPO", "").equals("op ecologico")) {
-                        z.setClass(getApplicationContext(), HomepageOperatoreEcologico.class);
-                    } else if (prefs.getString("TIPO", "").equals("dip comunale")) {
-                        z.setClass(getApplicationContext(), HomepageDipendenteComunale.class);
-                    }
-
-                } else {
-                    z.setClass(getApplicationContext(), LoginForm.class);
-
+            Intent z = new Intent();
+            if (prefs.getBoolean("RIMANI_CONNESSO", false) == true) {
+                if (prefs.getString("TIPO", "").equals("cittadino")) {
+                    z.setClass(getApplicationContext(), HomepageCittadino.class);
+                } else if (prefs.getString("TIPO", "").equals("op ecologico")) {
+                    z.setClass(getApplicationContext(), HomepageOperatoreEcologico.class);
+                } else if (prefs.getString("TIPO", "").equals("dip comunale")) {
+                    z.setClass(getApplicationContext(), HomepageDipendenteComunale.class);
                 }
-                startActivity(z);
+
+            } else {
+                z.setClass(getApplicationContext(), LoginForm.class);
+
             }
-
-
+            startActivity(z);
         }
 
 
     }
+
+
+}
